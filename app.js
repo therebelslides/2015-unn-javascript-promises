8bab8f6b58fd7e9d852e208cad38e12806a5b52e
Trent Oswald <trentoswald@therebelrobot.com>
2015-07-02 10:41:48 -0600

    cleanup

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 var request = require('unirest')
+
 var userData = {
   id: '516fba70-20d0-11e5-b5f7-727283247c7f',
   name: 'Frederick Bombastion'
 }
 
-var allResults = []
 getUserData(userData)
 
 function getUserData(userData) {
