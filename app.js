9099a8879379e5a90a0f280c5b3c5e6a2b53e68b
Trent Oswald <trentoswald@therebelrobot.com>
2015-07-02 10:41:13 -0600

    simplify

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
 var request = require('unirest')
+
 var userData = {
   id: '516fba70-20d0-11e5-b5f7-727283247c7f',
   name: 'Frederick Bombastion'
 }
 
 var allResults = []
+
 getUserData(userData)
 
 function getUserData(userData) {
-  var self = {userData:userData}
-  return new Promise(_getUserData.bind(self))
+  return new Promise(_getUserData)
 }
 
 function _getUserData(resolve, reject) {
-  var userData = this.userData
   request.get('http://mockbin.com/request?username=' + userData.name)
     .end(function (results) {
       if (results.error) {
