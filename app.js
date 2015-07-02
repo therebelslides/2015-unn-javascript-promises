4293c77b1c8596714fbce136ea40a61272b79e8a
Trent Oswald <trentoswald@therebelrobot.com>
2015-07-02 10:39:24 -0600

    simplify

--- app.js
+++ app.js
@@ -1,22 +1,21 @@
 var request = require('unirest')
+
 var userData = {
   id: '516fba70-20d0-11e5-b5f7-727283247c7f',
   name: 'Frederick Bombastion'
 }
 
 var allResults = []
-var self = {userData:userData}
+
 getUserData(userData)
-  .then(afterGetData.bind(self))
-  .catch(errorHandler.bind(self))
+  .then(afterGetData)
+  .catch(errorHandler)
 
 function getUserData(userData) {
-  var self = {userData:userData}
-  return new Promise(_getUserData.bind(self))
+  return new Promise(_getUserData)
 }
 
 function _getUserData(resolve, reject) {
-  var userData = this.userData
   request.get('http://mockbin.com/request?username=' + userData.name)
     .end(function (results) {
       if (!results.error) {
@@ -27,7 +26,6 @@ function _getUserData(resolve, reject) {
 }
 
 function afterGetData(results) {
-  var self = this
   allResults.push(results)
   console.log('Results:', allResults)
 }
